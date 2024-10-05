027d0c55c4c319da40c543ecd6b1ed7e27f30d98
Vsevolod78 <sevad1978@gmail.com>
2024-10-05 18:44:40 +0300

    Comments removed

--- homework/vsevolod_dudkov/homework_3/exercise_3.py
+++ homework/vsevolod_dudkov/homework_3/exercise_3.py
@@ -1,8 +1,6 @@
-#Поиск среднего арифметического и среднего геометрического
-#двух чисел
 
 number_one = 3
 number_two = 7
 
 print((number_one + number_two) / 2)
-print((number_one * number_two)**0.5)
+print((number_one * number_two) ** 0.5)
