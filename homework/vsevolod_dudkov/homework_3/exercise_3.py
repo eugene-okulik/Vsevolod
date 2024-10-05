4e6b1c25332406e8754e699c88d6cd9262eb0982
Vsevolod78 <sevad1978@gmail.com>
2024-10-05 18:23:25 +0300

    Added files from homework 3

---
+++ homework/vsevolod_dudkov/homework_3/exercise_3.py
@@ -0,0 +1,8 @@
+#Поиск среднего арифметического и среднего геометрического
+#двух чисел
+
+number_one = 3
+number_two = 7
+
+print((number_one + number_two) / 2)
+print((number_one * number_two)**0.5)
---
+++ homework/vsevolod_dudkov/homework_3/exercise_4.py
@@ -0,0 +1,10 @@
+#Нахождение гипотенузы и площади прямоугольного треугольника
+
+leg_one = 15
+leg_two = 25
+
+hypotenuse = ((leg_one ** 2) + (leg_two ** 2)) ** 0.5
+square = (leg_one * leg_two) / 2
+
+print(hypotenuse)
+print(square)
