07b64f28cfee09447a66cb28a3b15fccd747903e
Vsevolod78 <sevad1978@gmail.com>
2024-10-09 11:19:21 +0300

    Added file from homework 4Fixed indentations

--- homework/vsevolod_dudkov/homework_4/exercise_5.py
+++ homework/vsevolod_dudkov/homework_4/exercise_5.py
@@ -1,15 +1,14 @@
-
 my_dict = {'tuple': '', 'list': '', 'dict': '', 'set': ''}
 
 my_dict['tuple'] = (1, 2, 3, 4, 5)
 my_dict['list'] = [0.25, 0.3, 0.5, 1, 1.5]
 my_dict['dict'] = {
-                    'fruit': 'apple',
-                    'fruit1': 'banana',
-                    'fruit2': 'orange',
-                    'fruit3': 'peach',
-                    'fruit4': 'grape'
-                }
+    'fruit': 'apple',
+    'fruit1': 'banana',
+    'fruit2': 'orange',
+    'fruit3': 'peach',
+    'fruit4': 'grape'
+}
 my_dict['set'] = {'window', 'door', 'floor', 'wall', 'ceiling'}
 
 print(my_dict['tuple'][-1])
