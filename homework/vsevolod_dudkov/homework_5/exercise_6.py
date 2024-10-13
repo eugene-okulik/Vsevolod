c4947fa12ccbc8c615faeb198e3e9a62b013b63f
Vsevolod78 <sevad1978@gmail.com>
2024-10-13 12:25:45 +0300

    Added update homework 5

--- homework/vsevolod_dudkov/homework_5/exercise_6.py
+++ homework/vsevolod_dudkov/homework_5/exercise_6.py
@@ -1,9 +1,4 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
 
-name = person[0]
-last_name = person[1]
-city = person[2]
-phone = person[3]
-country = person[4]
-
-# print(name, last_name, city, phone, country)
+print(name, last_name, city, phone, country)
--- homework/vsevolod_dudkov/homework_5/exercise_7.py
+++ homework/vsevolod_dudkov/homework_5/exercise_7.py
@@ -2,13 +2,19 @@ result_one = 'результат операции: 42'
 result_two = 'результат операции: 514'
 result_three = 'результат работы программы: 9'
 
-slice_one = result_one[-2:]
-sum_one = int(slice_one) + 10
+result_index_one = result_one.index(':')
+slice_one = result_one[result_index_one:]
+slice_one_strip = slice_one.strip(': ')
+sum_one = int(slice_one_strip) + 10
 
-slice_two = result_two[-3:]
-sum_two = int(slice_two) + 10
+result_index_two = result_two.index(':')
+slice_two = result_two[result_index_two:]
+slice_two_strip = slice_two.strip(': ')
+sum_two = int(slice_two_strip) + 10
 
-slice_three = result_three[-1:]
-sum_three = int(slice_three) + 10
+result_index_three = result_three.index(':')
+slice_three = result_three[result_index_three:]
+slice_three_strip = slice_three.strip(': ')
+sum_three = int(slice_three_strip) + 10
 
-print(slice_one, sum_two, sum_three)
+print(sum_one, sum_two, sum_three)
