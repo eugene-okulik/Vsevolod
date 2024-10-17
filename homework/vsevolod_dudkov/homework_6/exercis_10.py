c61bba76c0d18ea114d28a52b9e9fe708d1757ac
Vsevolod78 <sevad1978@gmail.com>
2024-10-17 20:02:10 +0300

    Added changes to homework 6

--- homework/vsevolod_dudkov/homework_6/exercis_10.py
+++ homework/vsevolod_dudkov/homework_6/exercis_10.py
@@ -1,9 +1,9 @@
 for number in range(1, 101):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+        print('FuzzBuzz')
+    elif number % 3 == 0:
         print('fuzz')
     elif number % 5 == 0:
         print('buzz')
-    elif number % 3 == 0 and number % 5 == 0:
-        print('FuzzBuzz')
     else:
         print(number)
