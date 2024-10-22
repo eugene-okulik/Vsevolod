7a21356d259b29c6bd9662faf4acbe8c93065945
Vsevolod78 <sevad1978@gmail.com>
2024-10-22 17:21:58 +0300

    Files fixed

--- homework/vsevolod_dudkov/homework_7/exercise_12.py
+++ homework/vsevolod_dudkov/homework_7/exercise_12.py
@@ -1,4 +1,4 @@
 words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
-for key, number in words.items():
-    print(key * number)
+for keys, number in words.items():
+    print(keys * number)
--- homework/vsevolod_dudkov/homework_7/exercise_13.py
+++ homework/vsevolod_dudkov/homework_7/exercise_13.py
@@ -3,13 +3,15 @@ result_two = 'результат операции: 54'
 result_three = 'результат работы программы: 209'
 result_four = 'результат: 2'
 
-def sum(result, degree=10):
+
+def sum_repeat(result, degree=10):
     index_all = result.index(':')
     slice_all = result[index_all:]
     sl_str_one = slice_all.strip(': ')
     return int(sl_str_one) + degree
 
-print(sum(result_one))
-print(sum(result_two))
-print(sum(result_three))
-print(sum(result_four))
+
+print(sum_repeat(result_one))
+print(sum_repeat(result_two))
+print(sum_repeat(result_three))
+print(sum_repeat(result_four))
